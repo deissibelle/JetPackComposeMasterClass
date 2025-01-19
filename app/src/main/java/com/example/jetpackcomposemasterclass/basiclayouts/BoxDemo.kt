3e1dc9d3665b7db918eb281d659b814024d69ad0
deissibelle <deissibelle@gmail.com>
2025-01-19 19:27:11 +0100

    Box layouts

--- app/src/main/java/com/example/jetpackcomposemasterclass/basiclayouts/BoxDemo.kt
+++ app/src/main/java/com/example/jetpackcomposemasterclass/basiclayouts/BoxDemo.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcomposemasterclass.basiclayouts
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,34 +24,38 @@ import com.example.jetpackcomposemasterclass.ui.theme.JetPackComposeMasterClassT
 fun BoxDemo(modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier
-            .size(200.dp),
-        contentAlignment= Alignment.BottomCenter
+            .size(100.dp),
+        contentAlignment= Alignment.Center
     ) {
-
+Box(
+    modifier = Modifier
+        .matchParentSize()
+        .background(brush = Brush.verticalGradient(
+        colors = listOf(
+            Color.Transparent,
+            Color.Black
+        )
+    ))
+)
         Image(
             painter = painterResource(R.drawable.nature),
             contentDescription = null,
-            modifier = Modifier.size(300.dp)
+            modifier = Modifier.size(100.dp)
             
         )
         IconButton(
-            onClick = {}
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .size(10.dp)
         ) {
             Icon(
                 imageVector= Icons.Default.FavoriteBorder,
                 contentDescription=null,
-                tint = Color.White
+                tint = Color.Red
             )
         }
-        Text(
-            text="Hello Sibelle",
-            modifier = Modifier.align(Alignment.TopStart)
-        )
 
-        Text(
-            text="What's up?",
-
-            )
     }
 
 
